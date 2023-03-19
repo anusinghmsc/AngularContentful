a8d7026caa200f0ef7e97b0ee87af8e0d22af326
Anu Singh <anusinghmsc@gmail.com>
2023-03-19 19:30:37 +0000

    update

--- src/app/brand-list/brand-list.component.ts
+++ src/app/brand-list/brand-list.component.ts
@@ -9,6 +9,9 @@ import { Entry } from 'contentful';
   styleUrls: ['./brand-list.component.css'],
 })
 export class BrandListComponent implements OnInit {
+  // Map to hold selection status (true/false) for each brand id
+  selectedBrandsMap: Map<string, boolean> = new Map();
+
   brands: Entry<any>[] = []; // Array of Entry or Content in Contentful to hold data received from Contentful
 
   // brands = Argos, Tesco, Currys, Sainsburys, Lidl
@@ -16,7 +19,7 @@ export class BrandListComponent implements OnInit {
   constructor(
     private router: Router, // inject all dependencies for creating Router object
     private contentfulService: ContentfulService // inject all dependencies for creating Contentful Service object
-  ) {}
+  ) { }
 
   ngOnInit() {
     // get all the brands from Contentful and assign to brands variable 
@@ -30,5 +33,15 @@ export class BrandListComponent implements OnInit {
   // with brandID passed on clicking Brand Overview button 
   goToBrandOverviewPage(brandId: any) {
     this.router.navigate(['/brand', brandId]);
+  }
+
+  // update brand selection status from false to true and true to false on click
+  updateSelection(id: string) {
+    this.selectedBrandsMap.set(id, !this.selectedBrandsMap.get(id));
+  }
+
+  // get brand selection status as true or false
+  isSelected(id: string) {
+    return this.selectedBrandsMap.get(id);
   }
 }
